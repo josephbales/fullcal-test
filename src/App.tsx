41bc83adf18259771f2d4759e395a1d0e5a4c9e9
Joseph Bales <joseph.bales@gmail.com>
2022-07-26 21:15:02 -0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import FullCalendar from '@fullcalendar/react' // must go before plugins
+import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FullCalendar
+        plugins={[dayGridPlugin]}
+        initialView="dayGridMonth"
+        headerToolbar={{
+          start: 'today prev,next',
+          center: 'title',
+          end: ''
+        }}
+        viewDidMount={(arg) => {
+          // You can hook in here and render whatever content you want to the calendar view.
+          // For some reason it fires twice???  Fullcalendar is sometimes janky.
+          let test = document.getElementById('hiya-test');
+          if (!test) {
+            const x = document.getElementsByClassName("fc-header-toolbar");
+            const y = x[0];
+            const z = document.createElement('span');
+            z.setAttribute('id', 'hiya-test');
+            z.innerText = 'hiya';
+            y.append(z);
+          }
+        }}
+      />
     </div>
   );
 }
